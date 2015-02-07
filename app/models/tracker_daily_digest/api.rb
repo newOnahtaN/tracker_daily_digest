e7109c94fdd3ab32300a0a3d20060accaaaf2395
Karl Baum <karl.baum@gmail.com>
2015-02-06 21:19:28 -0500

    grabbing stories from every project now

--- app/models/tracker_daily_digest/api.rb
+++ app/models/tracker_daily_digest/api.rb
@@ -1,22 +1,25 @@
 module TrackerDailyDigest
   module Api
 
+    SUPPORT="1062572"
+    GENERAL="1255296"
+    WEBAPP="1047348"
+    HORSE="1239932"
+    INTEGRATIONS="1239852"
+
+    PROJECTS = [ SUPPORT, GENERAL, WEBAPP, HORSE, INTEGRATIONS ].freeze
+
     def self.client
       @client ||= TrackerApi::Client.new(token: ENV['TRACKER_TOKEN'])
     end
 
     def self.memberships
-      TrackerApi::Endpoints::Memberships.new(client).get(1047348)
+      TrackerApi::Endpoints::Memberships.new(client).get(GENERAL)
     end
 
     def self.stories initials
-      project.stories(filter:"state:delivered, requester:#{initials}")
+      PROJECTS.flat_map{ |project| client.project(project).stories(filter:"state:delivered, requester:#{initials}") }
     end
-
-    def self.project
-      client.project(1047348)
-    end
-
 
   end
 end
