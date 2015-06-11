e4d3a0323ed5e67ba0b55bc0161840e764ac79f0
newOnahtaN <ncowen@email.wm.edu>
2015-06-11 19:32:44 -0400

    updated html

--- app/mailers/delivered_pivotal_stories_mailer.rb
+++ app/mailers/delivered_pivotal_stories_mailer.rb
@@ -2,6 +2,10 @@ class DeliveredPivotalStoriesMailer < ActionMailer::Base
   default from: "no-reply@viewthespace.com"
 
   def delivered_pivotal_stories_mailer new_stories
+    attachments.inline['bug'] = File.read('app/assets/images/bug.png')
+    attachments.inline['chore'] = File.read('app/assets/images/chore.png')
+    attachments.inline['feature'] = File.read('app/assets/images/feature.png')
+    attachments.inline['release'] = File.read('app/assets/images/release.png')
     @new_stories = new_stories
     unless (email_recipient = ENV["EMAIL_RECIPIENT"]).nil?
       mail(to: email_recipient , subject: 'Updates Since Last Deployment')
