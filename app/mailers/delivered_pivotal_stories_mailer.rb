8be3189404da0b3757ac87fd6e8463baf70fb8c5
newOnahtaN <ncowen@email.wm.edu>
2015-06-12 11:47:26 -0400

    Fixed encoding problem

--- app/mailers/delivered_pivotal_stories_mailer.rb
+++ app/mailers/delivered_pivotal_stories_mailer.rb
@@ -2,10 +2,7 @@ class DeliveredPivotalStoriesMailer < ActionMailer::Base
   default from: "no-reply@viewthespace.com"
 
   def delivered_pivotal_stories_mailer new_stories, date_string
-    attachments.inline['bug'] = File.read('app/assets/images/bug.png')
-    attachments.inline['chore'] = File.read('app/assets/images/chore.png')
-    attachments.inline['feature'] = File.read('app/assets/images/feature.png')
-    attachments.inline['release'] = File.read('app/assets/images/release.png')
+    attach_images
     @new_stories = new_stories
     @time_of_last_deployment = DateTime.parse(date_string)
     unless (email_recipient = ENV["EMAIL_RECIPIENT"]).nil?
@@ -14,4 +11,16 @@ class DeliveredPivotalStoriesMailer < ActionMailer::Base
       mail(to: 'nathan.owen@viewthespace.com' , subject: 'Updates Since Last Deployment')
     end
   end
+
+  def attach_images
+    attachments.inline['bug'] = {:data => File.read('app/assets/images/bug.png'),
+                                 :mime_type => "image/png"}
+    attachments.inline['chore'] = {:data => File.read('app/assets/images/chore.png'),
+                                   :mime_type => "image/png"}
+    attachments.inline['feature'] = {:data => File.read('app/assets/images/feature.png'),
+                                     :mime_type => "image/png"}
+    attachments.inline['release'] = {:data => File.read('app/assets/images/release.png'),
+                                     :mime_type => "image/png"}
+  end
+
 end
